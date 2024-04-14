8dca116ccfee658acb457c3689ba3e129a8986ba
Veronica H <vth@Veronicas-MacBook-Pro.local>
2024-04-14 18:19:28 -0400

    setup questions data structure + question page's data

--- code/server.py
+++ code/server.py
@@ -6,6 +6,68 @@ app = Flask(__name__)
 
 import re
 
+# DATA
+
+#question bank
+#q will be the question
+#media will be any necessary media
+#a will be the bank of possible answers
+#a_correct will be the index of the correct answer
+#TO DO: 1) should id and a_correct be strings or ints? 2) add in real q and a
+question = [
+   {
+      "id": "1",
+      "q": "asghjoieajsgd",
+      "media": "",
+      "a": ["weretygh", "yrterds", "wretry"],
+      "a_correct": 1
+   },
+   {
+      "id": "2",
+      "q": "wgrhifp;ovleas",
+      "media": "",
+      "a": ["ytre", "waretyu", "iujhygrfed"],
+      "a_correct": 3
+   },
+   {
+      "id": "3",
+      "q": "qwaresty",
+      "media": "",
+      "a": ["wearestryfy", "ikujyhtg", "ujyh5tg4r"],
+      "a_correct": 2
+   },
+   {
+      "id": "4",
+      "q": "rethgjh",
+      "media": "",
+      "a": ["retyuy", "kujhygtf", "arestdyf"],
+      "a_correct": 1
+   }
+]
+
+#user's quiz selections stored here
+#a is their selection
+user_quiz_data = [
+   {
+      "id": "1",
+      "a": ""
+   },
+   {
+      "id": "2",
+      "a": ""
+   },
+   {
+      "id": "3",
+      "a": ""
+   },
+   {
+      "id": "4",
+      "a": ""
+   }
+]
+
+num_of_questions = 4
+
 
 
 
@@ -26,12 +88,13 @@ def quiz():
 # display quiz question #
 @app.route('/quiz/<int:quiz_id>')
 def pageview(quiz_id):
-    if quiz_id > 5:
+    if quiz_id > num_of_questions:
         return "Question not found", 404
     else:
-        # object = data[restaurant_id-1]   # -1 to compensate for indexing starting at 0
-        # edit_link = "/edit/" + str(quiz_id)
-        return render_template('quiz.html')
+        object = question[quiz_id-1]   # -1 to compensate for indexing starting at 0
+        quiz_id = quiz_id+1
+        next_link = "/quiz/" + str(quiz_id) #link to next quiz question
+        return render_template('quiz.html', object=object, user_quiz_data=user_quiz_data, next_link=next_link)
 
 
 @app.route('/quizresult')
