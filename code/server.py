1b60249e535c0210363ba1f01320ca30ddb0ad9c
Veronica H <vth@Veronicas-MacBook-Pro.local>
2024-04-14 16:30:04 -0400

    added quiz question pages

--- code/server.py
+++ code/server.py
@@ -21,7 +21,22 @@ def learn():
 
 @app.route('/quiz')
 def quiz():
-   return render_template('quiz.html')   
+   return render_template('quiz.html')
+
+# display quiz question #
+@app.route('/quiz/<int:quiz_id>')
+def pageview(quiz_id):
+    if quiz_id > 5:
+        return "Question not found", 404
+    else:
+        # object = data[restaurant_id-1]   # -1 to compensate for indexing starting at 0
+        # edit_link = "/edit/" + str(quiz_id)
+        return render_template('quiz.html')
+
+
+@app.route('/quizresult')
+def quizresult():
+   return render_template('quizresult.html')   
 
 
 
