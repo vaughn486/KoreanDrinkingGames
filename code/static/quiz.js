5a5da78ccbec06312371e9bb4665cef651213abb
Grace Chae-Yeon Huh <gch2126@columbia.edu>
2024-04-29 16:29:12 -0400

    gives red/green feedback

--- code/static/quiz.js
+++ code/static/quiz.js
@@ -2,23 +2,25 @@ $(document).ready(function(){
     var correct_answer = question_data.a_correct;
 
 
-    $(".quizbutton").click(function() {
+    $(".quiz-question").click(function() {
         // Get the ID of the clicked button
         var buttonId = $(this).attr("id");
         
         if(parseInt(buttonId) === correct_answer){
-            $(this).css("background-color", "green");
+            $(this).css("background-color", "rgb(139, 176, 142)");
+            $(this).css("border", "0px solid rgb(139, 176, 142");
 
         }else{
-            $(this).css("background-color", "red");
+            $(this).css("background-color", "rgb(214, 144, 144)");
+            $(this).css("border", "0px solid rgb(214, 144, 144");
         }
     });
 
     
     
 })
-    
-    
+
+
     
     
     
